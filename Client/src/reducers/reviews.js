03d0140064b33655b67539bcab0f10c76be200d6
TheMysticalBard <parker.smyrl@gmail.com>
2021-05-01 18:26:21 -0500

    Fixed the bug poggy pogs

--- Client/src/reducers/reviews.js
+++ Client/src/reducers/reviews.js
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (reviews = [], action) => {
-    switch(action.type){
+    switch(action.type) {
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
             return [...reviews, action.payload];
         default:
             return reviews;
-        }
+    }
 }
